b7f00305a365c4d0efdc1451082b5e9ec1e631b9
Sergei <Yozhig@users.noreply.github.com>
2019-03-29 19:47:15 +0300

    CAPI-340: add protocol definition (#1)

---
+++ proto/bender.thrift
@@ -0,0 +1,35 @@
+namespace java com.rbkmoney.bender
+namespace erlang bender
+
+include "proto/msgpack.thrift"
+
+typedef string ExternalID
+typedef string InternalID
+
+struct GenerationResult {
+    1: required InternalID internal_id
+    2: optional msgpack.Value context
+}
+
+union GenerationSchema {
+    1: SnowflakeSchema snowflake
+    2: ConstantSchema constant
+    3: SequenceSchema sequence
+}
+
+struct SnowflakeSchema {
+}
+
+struct ConstantSchema {
+    1: required InternalID internal_id
+}
+
+struct SequenceSchema {
+    1: required string sequence_id
+}
+
+service Bender {
+
+    GenerationResult GenerateID (1: ExternalID external_id, 2: GenerationSchema schema, 3: msgpack.Value context)
+
+}
