75be9417ba704326c5711f935f8745d504d92935
Andrew Mayorov <a.mayorov@rbkmoney.com>
2021-06-30 17:13:22 +0300

    Sprinkle with docs (#13)

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 [![Build Status](http://ci.rbkmoney.com/buildStatus/icon?job=rbkmoney_private/bender-proto/master)](http://ci.rbkmoney.com/job/rbkmoney_private/job/bender-proto/job/master/)
 
-Thrift protocol for bender service
+An _external identifier binding_ service protocol. Implemented by [bender](https://github.com/rbkmoney/bender).
+
+## What?
+
+Glad you asked. This protocol describes a service which deals with _binding_ abstraction. A _binding_ ties some _external identifier_ (e.g. identifier of some object or process in some third-party system) with a single uniform and predictable _internal identifier_, and does this _uniquely_: there should no more than one such binding for each external identifier.
+
+## Why?
+
+* You want to guarantee _idempotency_ of your system.
+
+    You should tell your clients to include some form of _external identifier_ or _idempotency key_ when interacting with your system's API. This service will make sure that you'll end up with the same _internal identifier_, no matter how many identical requests you end up receiving.
+
+* ...And also you want _predictable_ identifiers.
+
+    In other words you want to have, for example, plain 64-bit counter for an identifier internally, no matter how diverse your clients' identifiers are.
+
+* ...And also you want to spot _conflicting_ requests with same external identifiers.
+
+    For that you might compute, for example, some stable hash of the request data and associate it with a binding as a _context_. Service guarantees that only the first actor who actually creates a binding (the _winner_) will have its context persisted. Other actors will see winner's context in service responses.
--- proto/bender.thrift
+++ proto/bender.thrift
@@ -3,8 +3,26 @@ namespace erlang bender
 
 include "proto/msgpack.thrift"
 
+/**
+* Identifier of some process, actor or object in an external system.
+*
+* Normally, each `ExternalID` is composed of some identifier of an external system, a type or class
+* of entity being identified, and finally this entity's identifier.
+* For example: "CP/DigitalOcean/Droplet/92878506-0072-4d3e-9f55-2285edad69bf" uniquely identifies
+* specific droplet in the DigitalOcean's cloud.
+*/
 typedef string ExternalID
+
+/**
+* Identifier of respective process, actor or object in the local system, to be used internally.
+* Uniquely bound with a single external identifier.
+*/
 typedef string InternalID
+
+/**
+* Internal identifier in the form of a signed integer.
+* Note that negative values are possible.
+*/
 typedef i64    IntegerInternalID
 
 exception InternalIDNotFound {}
@@ -26,33 +44,74 @@ struct GetInternalIDResult {
     3: optional IntegerInternalID integer_internal_id
 }
 
+/**
+* Schema which tells how to generate an internal identifier.
+*/
 union GenerationSchema {
     1: SnowflakeSchema snowflake
     2: ConstantSchema constant
     3: SequenceSchema sequence
 }
 
+/**
+* Internal identifer will be generated following the Snowflake algorithm.
+*
+* Such ID will have the form `<Time, MachineID, SequenceID>' where:
+* - `Time' is a 42 bit binary integer recording milliseconds since UTC 2012-01-01T00:00:00Z,
+* - `MachineID' is a 10 bit integer identifying the Snowflake machine which generated the said ID,
+* - and `SequenceID' is a 12 bit integer counting the number of IDs generated on this server,
+*   this millisecond.
+*/
 struct SnowflakeSchema {
 }
 
+/**
+* Internal identifier won't be generated, but picked as specified.
+*/
 struct ConstantSchema {
+    /// Which internal identifier this binding will have?
     1: required InternalID internal_id
 }
 
+/**
+* Internal identifier will be the next value of the specified sequence.
+*
+* Such IDs are more user-friendly yet cost a bit more to generate.
+*/
 struct SequenceSchema {
+    /// Which sequence to pick value from?
     1: required string sequence_id
+    /// Internal ID will be no less than that.
+    /// If current value of the sequence is less than values up to `minimum` will be skipped.
     2: optional i64 minimum
 }
 
 service Bender {
 
+    /**
+    * Generate unique internal identifier bound to the given external identifier.
+    *
+    * This operation will not fail, instead you always get details of the original binding in
+    * response. Which means that, for example, you might get a Snowflake ID in response while
+    * asking for next sequence value, because original binding was created with Snowflake schema.
+    * If you happen to be the one who initially does the binding, you are the winner and your
+    * `context` will be associated with that binding. If not, you will see the **context of the
+    * winner** in response, not yours.
+    */
     GenerationResult GenerateID (1: ExternalID external_id, 2: GenerationSchema schema, 3: msgpack.Value context)
 
+    /**
+    * Get binding details given external identifier.
+    *
+    * This operation will fail when there's no such binding, in contrast to `GenerateID`.
+    */
     GetInternalIDResult GetInternalID (1: ExternalID external_id)
         throws (1: InternalIDNotFound ex1)
 }
 
 service Generator {
-
+    /**
+    * Generate unique internal identifier.
+    */
     GeneratedID GenerateID (1: GenerationSchema schema)
 }
